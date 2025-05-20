38c4901903be014959565a766ef1020a9d2e59aa
Arch me <kishudwivedi30@gmail.com>
2025-05-20 11:06:21 +0530

    23424

--- server.c
+++ server.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <netinet/ip.h>
+#include <unistd.h>
 
 int main() {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -18,4 +20,13 @@ int main() {
     printf("SO_REUSEADDR failed: %s \n", strerror(errno));
     return 1;
   }
+
+  struct sockaddr_in serv_addr = {
+    .sin_family = AF_INET , 
+    .sin_port = htons(6969),
+    .sin_addr = { htonl(INADDR_ANY) },
+  };
+
+
+
 }
