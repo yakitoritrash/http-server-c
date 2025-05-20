22b9a75f1bcaefdb823e582c5120ceed0203730d
Arch me <kishudwivedi30@gmail.com>
2025-05-20 09:36:00 +0530

    3434

--- server.c
+++ server.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <errno.h>
 
 
 int main() {
-  int server_fd = socket(AF_INET, 
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0); 
+  if (server_fd < 0) {
+    perror ("connection failed\n");
+  }
+  printf("Socket created.\n");
 }
