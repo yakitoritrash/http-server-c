d6015ee95bd34266878ade032e19f49d9152c2a5
Arch me <kishudwivedi30@gmail.com>
2025-05-20 10:08:48 +0530

    3434

--- server.c
+++ server.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <errno.h>
-
+#include <netinet/in.h>
 
 int main() {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -9,5 +9,7 @@ int main() {
     perror("Socket creation failed.\n");
   }
   printf("Socket created.\n");
+  
+  struct sockaddr_in client_addr;
 
 }
