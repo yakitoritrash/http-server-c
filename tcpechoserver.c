930609057f93d5c57d416f77ea28dd1b6d135dc3
Arch me <kishudwivedi30@gmail.com>
2025-09-03 17:39:19 +0530

    c web server

--- tcpechoserver.c
+++ tcpechoserver.c
@@ -17,7 +17,6 @@
 //once connected data is transferred using read(), write() or variants of send(), recv()
 //when session complete close()
 
-
 int main() {
   //creating a socket
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -56,17 +55,54 @@ int main() {
     int client_fd = accept(server_fd, &CLIENT_ADDR, &client_addr_len); 
     int pid = fork();
     if (pid == 0) {
-      char buffer[1024];
-      ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer));
-      printf("--- Browser Request ---\n%s\n", buffer);
-      char *http_response = 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Content-Length: 45\r\n"
-        "\r\n"
-        "<h1>Hello from c server</h1>";
+      char *err404 = "HTTP/1.1 404 Not Found\r\nConten-Length: 0\r\n\r\n";
+      char request_buffer[2048];
+      ssize_t bytes_read = read(client_fd, request_buffer, sizeof(request_buffer));
+      //printf("%s\n", request_buffer);
+      char* method = strtok(request_buffer, " ");
+      char* path = strtok(NULL, " ");
+      char* filename;
+      char* content_type;
+      if (strcmp(path, "/") == 0) {
+        filename = "index.html";
+        content_type = "text/html";
+      } else if (strcmp(path, "/image.jpg") == 0) {
+        filename = "image.jpg";
+        content_type = "image/jpeg";
+      } else {
+        write(client_fd, err404, strlen(err404));
+        exit(0);
+      }
+      FILE *ptr = fopen(filename, "r");
+      if (ptr == NULL) {
+        perror("File not found.\n");
+        write(client_fd, err404, strlen(err404));
+        exit(0);
+      }
+      fseek(ptr, 0, SEEK_END);
+      long a = ftell(ptr);
+      rewind(ptr);
+      //char buffer[1024];
+      //ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer));
+      char* buffer = malloc(a);
+      fread(buffer, 1, a, ptr);
+      fclose(ptr);
+      char header_buffer[1024];
+      int header_len = sprintf(header_buffer, "HTTP/1.1 200 OK\r\n"
+                                  "Content-Type: %s\r\n"
+                                  "Content-Length: %ld\r\n\r\n",content_type, a);
+      //printf("--- Browser Request ---\n%s\n", buffer);
+      //char *http_response = 
+        //"HTTP/1.1 200 OK\r\n"
+        //"Content-Type: text/html\r\n"
+        //"Content-Length: 45\r\n"
+        //"\r\n";
+        //"<h1>Hello from c server</h1>";
       //write(client_fd, buffer, bytes_read);
-      write(client_fd, http_response, strlen(http_response));
+      //write(client_fd, http_response, strlen(http_response));
+      write(client_fd, header_buffer, header_len);
+      write(client_fd, buffer, a);
+      free(buffer);
       close(client_fd);
       exit(0);
     } else if (pid > 0) {
