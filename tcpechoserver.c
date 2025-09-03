68b83333a63e22efd20b47a6c0bb23d3def39068
Arch me <kishudwivedi30@gmail.com>
2025-09-03 09:17:43 +0530

    echo server

--- tcpechoserver.c
+++ tcpechoserver.c
@@ -20,12 +20,33 @@
 int main() {
   //creating a socket
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  printf("Socket created.\n");
   if (server_fd < 0) {
-    errno;
+    perror("Socket creation failed.\n");
   }
+  //defining the address
   struct sockaddr_in SOCK_ADDR;
   SOCK_ADDR.sin_family = AF_INET;
-  SOCK_ADDR.sin_port = 8080;
-  SOCK_ADDR.sin_addr = INADDR_ANY;
+  SOCK_ADDR.sin_port = htons(8080);
+  SOCK_ADDR.sin_addr.s_addr = INADDR_ANY;
 
+  //binding the address to teh socket
+  if (bind(server_fd, (struct sockaddr *)&SOCK_ADDR, sizeof(SOCK_ADDR)) != 0) {
+    perror("Bind failed\n");
+  } 
+  printf("Binded\n");
+
+  int backlog = 5;
+  if (listen(server_fd, backlog) != 0) {
+    perror("Listen failed\n");
+  }
+  printf("Listening..\n");
+  struct sockaddr CLIENT_ADDR;
+  socklen_t client_addr_len = sizeof(CLIENT_ADDR);
+  int client_fd = accept(server_fd, &CLIENT_ADDR, &client_addr_len); 
+  char buffer[1024];
+  ssize_t bytes_read = read(client_fd, buffer, sizeof(bytes_read));
+  write(client_fd, buffer, sizeof(bytes_read));
+  close(client_fd);
+  close(server_fd);
 }
